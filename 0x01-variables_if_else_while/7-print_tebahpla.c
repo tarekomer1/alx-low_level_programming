2db4e1065793caf3d2eefe845e0d0843701b7641
tarekomer1 <manofkeeping@gmail.com>
2023-02-17 07:24:51 -0800

    z

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	/* your code goes there */
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
