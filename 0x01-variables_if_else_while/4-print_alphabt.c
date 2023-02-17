a48d2f3bc9c003ea06a7bfe264d854530e6121f5
tarekomer1 <manofkeeping@gmail.com>
2023-02-17 07:01:18 -0800

    z

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	/* your code goes there */
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (n != 'e' && n != 'q')
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
