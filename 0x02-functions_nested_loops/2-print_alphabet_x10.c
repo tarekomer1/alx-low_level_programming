b08a8c73eac048b268f61a0d926fcfa562f9d814
tarekomer1 <manofkeeping@gmail.com>
2023-02-21 14:23:12 -0800

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	int i;
+	i=0;
+	while( i<10)
+	{
+		char c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+
+}
