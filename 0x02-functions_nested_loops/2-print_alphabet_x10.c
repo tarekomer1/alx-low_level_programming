24294f195f9455154025395b19b00a88f4154c1b
tarekomer1 <manofkeeping@gmail.com>
2023-02-21 15:02:00 -0800

    m

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,16 @@
 
 void print_alphabet_x10(void)
 {
+
 	int i;
+
 	i = 0;
 
-	while(i < 10)
+	while( i < 10)
 	{
 		char c = 'a';
 
-		while (c <= 'z')
+		while ( c <= 'z')
 		{
 			_putchar(c);
 			c++;
